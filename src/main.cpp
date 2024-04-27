c6756608f0a6213d1e18981652e281707a33a66a
Q-engineering <info@qengineering.eu>
2024-04-27 18:00:25 +0200

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,190 @@
+// Copyright (c) 2021 by Rockchip Electronics Co., Ltd. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//
+// Modified by Q-engineering 4-6-2024
+//
+
+/*-------------------------------------------
+                Includes
+-------------------------------------------*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <cstdlib>                  // for malloc and free
+#include "opencv2/opencv.hpp"
+#include "opencv2/highgui.hpp"
+#include "yolov8_seg.h"
+#include "postprocess.h"
+#include "rknn_api.h"
+#include "rk_common.h"
+
+static const char* labels[] = {
+    "person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat", "traffic light",
+    "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow",
+    "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee",
+    "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard",
+    "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple",
+    "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "couch",
+    "potted plant", "bed", "dining table", "toilet", "tv", "laptop", "mouse", "remote", "keyboard", "cell phone",
+    "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear",
+    "hair drier", "toothbrush"
+};
+
+/*-------------------------------------------
+                  Main Functions
+-------------------------------------------*/
+int main(int argc, char** argv)
+{
+    float f;
+    float FPS[16];
+    int i, Fcnt=0;
+    std::chrono::steady_clock::time_point Tbegin, Tend;
+    char*          model_name = NULL;
+    const char*    imagepath = argv[1];
+    int            ret;
+
+    unsigned char class_colors[][3] = {
+        {255, 56, 56},   // 'FF3838'
+        {255, 157, 151}, // 'FF9D97'
+        {255, 112, 31},  // 'FF701F'
+        {255, 178, 29},  // 'FFB21D'
+        {207, 210, 49},  // 'CFD231'
+        {72, 249, 10},   // '48F90A'
+        {146, 204, 23},  // '92CC17'
+        {61, 219, 134},  // '3DDB86'
+        {26, 147, 52},   // '1A9334'
+        {0, 212, 187},   // '00D4BB'
+        {44, 153, 168},  // '2C99A8'
+        {0, 194, 255},   // '00C2FF'
+        {52, 69, 147},   // '344593'
+        {100, 115, 255}, // '6473FF'
+        {0, 24, 236},    // '0018EC'
+        {132, 56, 255},  // '8438FF'
+        {82, 0, 133},    // '520085'
+        {203, 56, 255},  // 'CB38FF'
+        {255, 149, 200}, // 'FF95C8'
+        {255, 55, 199}   // 'FF37C7'
+    };
+
+    for(i=0;i<16;i++) FPS[i]=0.0;
+
+    if (argc < 2) {
+        fprintf(stderr,"Usage: %s [imagepath] [model (optional)]\n", argv[0]);
+        return -1;
+    }
+
+    if (argc == 3) {
+        model_name = argv[2];
+    }
+    else{
+        model_name = (char*) "./model/yolov8s-seg.rknn";
+    }
+
+    rknn_app_context_t rknn_app_ctx;
+    memset(&rknn_app_ctx, 0, sizeof(rknn_app_context_t));
+
+    ret = init_yolov8_seg_model(model_name, &rknn_app_ctx);
+    if (ret != 0)
+    {
+        printf("init_yolov8_seg_model fail! ret=%d model_name=%s\n", ret, model_name);
+        return -1;
+    }
+
+    while(1){
+        cv::Mat orig_img;
+        orig_img=cv::imread(imagepath, 1);
+        if(orig_img.empty()) {
+            printf("Error grabbing\n");
+            break;
+        }
+
+        Tbegin = std::chrono::steady_clock::now();
+
+        object_detect_result_list od_results;
+
+        inference_yolov8_seg_model(&rknn_app_ctx, &orig_img, &od_results);
+
+        // draw mask
+        if (od_results.count >= 1){
+            int wd = orig_img.cols;
+            int ht = orig_img.rows;
+            uint8_t *seg_mask = od_results.results_seg[0].seg_mask;
+            for(int y = 0; y < ht; y++){
+                uint8_t *seg_mask_h = &(seg_mask[y*wd]);
+                for(int x = 0; x < wd; x++){
+                    if (seg_mask_h[x] != 0){
+                        int co = seg_mask_h[x] % 20;
+                        BGR &bgr = orig_img.ptr<BGR>(y)[x];
+                        bgr.b = cv::saturate_cast<uchar>(bgr.b * 0.5 + class_colors[co][0] * 0.5);
+                        bgr.g = cv::saturate_cast<uchar>(bgr.g * 0.5 + class_colors[co][1] * 0.5);
+                        bgr.r = cv::saturate_cast<uchar>(bgr.r * 0.5 + class_colors[co][2] * 0.5);
+                    }
+                }
+            }
+        }
+
+        // draw objects
+        char text[256];
+        for (int i = 0; i < od_results.count; i++) {
+            object_detect_result* det_result = &(od_results.results[i]);
+
+            int x1 = det_result->box.left;
+            int y1 = det_result->box.top;
+            int x2 = det_result->box.right;
+            int y2 = det_result->box.bottom;
+            cv::rectangle(orig_img, cv::Point(x1, y1), cv::Point(x2, y2),cv::Scalar(255, 0, 0));
+
+        //            printf("%s @ (%d %d %d %d) %f\n", det_result->name, det_result->box.left, det_result->box.top,
+        //                   det_result->box.right, det_result->box.bottom, det_result->prop);
+
+            //put some text
+            sprintf(text, "%s %.1f%%", labels[clamp(det_result->cls_id,0,80)], det_result->prop * 100);
+
+            int baseLine = 0;
+            cv::Size label_size = cv::getTextSize(text, cv::FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseLine);
+
+            int x = det_result->box.left;
+            int y = det_result->box.top - label_size.height - baseLine;
+            if (y < 0) y = 0;
+            if (x + label_size.width > orig_img.cols) x = orig_img.cols - label_size.width;
+
+            cv::rectangle(orig_img, cv::Rect(cv::Point(x, y), cv::Size(label_size.width, label_size.height + baseLine)), cv::Scalar(255, 255, 255), -1);
+
+            cv::putText(orig_img, text, cv::Point(x, y + label_size.height), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 0));
+        }
+
+        Tend = std::chrono::steady_clock::now();
+        //calculate frame rate
+        f = std::chrono::duration_cast <std::chrono::milliseconds> (Tend - Tbegin).count();
+        if(f>0.0) FPS[((Fcnt++)&0x0F)]=1000.0/f;
+        for(f=0.0, i=0;i<16;i++){ f+=FPS[i]; }
+        putText(orig_img, cv::format("FPS %0.2f", f/16),cv::Point(10,20),cv::FONT_HERSHEY_SIMPLEX,0.6, cv::Scalar(0, 0, 255));
+
+            //show output
+//        std::cout << "FPS" << f/16 << std::endl;
+        imshow("Radxa zero 3W - 1,8 GHz - 4 Mb RAM", orig_img);
+        char esc = cv::waitKey(2);
+        if(esc == 27) break;
+
+//      imwrite("./out.jpg", orig_img);
+    }
+
+    release_yolov8_seg_model(&rknn_app_ctx);
+
+    return 0;
+}
---
+++ src/yolov8_seg.cpp
@@ -0,0 +1,246 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#include "yolov8_seg.h"
+#include "rk_common.h"
+
+int read_data_from_file(const char *path, char **out_data)
+{
+    FILE *fp = fopen(path, "rb");
+    if(fp == NULL) {
+        printf("fopen %s fail!\n", path);
+        return -1;
+    }
+    fseek(fp, 0, SEEK_END);
+    size_t file_size = ftell(fp);
+    char *data = (char *)malloc(file_size+1);
+    data[file_size] = 0;
+    fseek(fp, 0, SEEK_SET);
+    if(file_size != fread(data, 1, file_size, fp)) {
+        printf("fread %s fail!\n", path);
+        free(data);
+        fclose(fp);
+        return -1;
+    }
+    if(fp) {
+        fclose(fp);
+    }
+    *out_data = data;
+    return file_size;
+}
+
+int init_yolov8_seg_model(const char *model_path, rknn_app_context_t *app_ctx)
+{
+
+    int ret;
+    int model_len = 0;
+    char *model;
+    rknn_context ctx = 0;
+
+    // Load RKNN Model
+    model_len = read_data_from_file(model_path, &model);
+    if (model == NULL)
+    {
+        printf("load_model fail!\n");
+        return -1;
+    }
+
+    ret = rknn_init(&ctx, model, model_len, 0, NULL);
+    free(model);
+    if (ret < 0)
+    {
+        printf("rknn_init fail! ret=%d\n", ret);
+        return -1;
+    }
+
+    // Get Model Input Output Number
+    rknn_input_output_num io_num;
+    ret = rknn_query(ctx, RKNN_QUERY_IN_OUT_NUM, &io_num, sizeof(io_num));
+    if (ret != RKNN_SUCC)
+    {
+        printf("rknn_query fail! ret=%d\n", ret);
+        return -1;
+    }
+    printf("model input num: %d, output num: %d\n", io_num.n_input, io_num.n_output);
+
+    // Get Model Input Info
+    printf("input tensors:\n");
+    rknn_tensor_attr input_attrs[io_num.n_input];
+    memset(input_attrs, 0, sizeof(input_attrs));
+    for (uint32_t i = 0; i < io_num.n_input; i++)
+    {
+        input_attrs[i].index = i;
+        ret = rknn_query(ctx, RKNN_QUERY_INPUT_ATTR, &(input_attrs[i]), sizeof(rknn_tensor_attr));
+        if (ret != RKNN_SUCC)
+        {
+            printf("rknn_query fail! ret=%d\n", ret);
+            return -1;
+        }
+        dump_tensor_attr(&(input_attrs[i]));
+    }
+
+    // Get Model Output Info
+    printf("output tensors:\n");
+    rknn_tensor_attr output_attrs[io_num.n_output];
+    memset(output_attrs, 0, sizeof(output_attrs));
+    for (uint32_t i = 0; i < io_num.n_output; i++)
+    {
+        output_attrs[i].index = i;
+        ret = rknn_query(ctx, RKNN_QUERY_OUTPUT_ATTR, &(output_attrs[i]), sizeof(rknn_tensor_attr));
+        if (ret != RKNN_SUCC)
+        {
+            printf("rknn_query fail! ret=%d\n", ret);
+            return -1;
+        }
+        dump_tensor_attr(&(output_attrs[i]));
+    }
+
+    // Set to context
+    app_ctx->rknn_ctx = ctx;
+
+    if (output_attrs[0].qnt_type == RKNN_TENSOR_QNT_AFFINE_ASYMMETRIC && output_attrs[0].type != RKNN_TENSOR_FLOAT16)
+    {
+        app_ctx->is_quant = true;
+    }
+    else
+    {
+        app_ctx->is_quant = false;
+    }
+
+    app_ctx->io_num = io_num;
+    app_ctx->input_attrs = (rknn_tensor_attr *)malloc(io_num.n_input * sizeof(rknn_tensor_attr));
+    memcpy(app_ctx->input_attrs, input_attrs, io_num.n_input * sizeof(rknn_tensor_attr));
+    app_ctx->output_attrs = (rknn_tensor_attr *)malloc(io_num.n_output * sizeof(rknn_tensor_attr));
+    memcpy(app_ctx->output_attrs, output_attrs, io_num.n_output * sizeof(rknn_tensor_attr));
+
+    if (input_attrs[0].fmt == RKNN_TENSOR_NCHW)
+    {
+        printf("model is NCHW input fmt\n");
+        app_ctx->model_channel = input_attrs[0].dims[1];
+        app_ctx->model_height = input_attrs[0].dims[2];
+        app_ctx->model_width = input_attrs[0].dims[3];
+    }
+    else
+    {
+        printf("model is NHWC input fmt\n");
+        app_ctx->model_height = input_attrs[0].dims[1];
+        app_ctx->model_width = input_attrs[0].dims[2];
+        app_ctx->model_channel = input_attrs[0].dims[3];
+    }
+    printf("model input height=%d, width=%d, channel=%d\n",
+           app_ctx->model_height, app_ctx->model_width, app_ctx->model_channel);
+
+    return 0;
+}
+
+int release_yolov8_seg_model(rknn_app_context_t *app_ctx)
+{
+    if (app_ctx->input_attrs != NULL)
+    {
+        free(app_ctx->input_attrs);
+        app_ctx->input_attrs = NULL;
+    }
+    if (app_ctx->output_attrs != NULL)
+    {
+        free(app_ctx->output_attrs);
+        app_ctx->output_attrs = NULL;
+    }
+    if (app_ctx->rknn_ctx != 0)
+    {
+        rknn_destroy(app_ctx->rknn_ctx);
+        app_ctx->rknn_ctx = 0;
+    }
+    return 0;
+}
+
+int inference_yolov8_seg_model(rknn_app_context_t *app_ctx, cv::Mat* org_img, object_detect_result_list *od_results)
+{
+    int ret;
+    cv::Mat img;
+    cv::Mat resized_img;
+    letterbox_t letter_box;
+    rknn_input inputs[app_ctx->io_num.n_input];
+    rknn_output outputs[app_ctx->io_num.n_output];
+    const float nms_threshold = NMS_THRESH;
+    const float box_conf_threshold = BOX_THRESH;
+    int         img_width          = 0;
+    int         img_height         = 0;
+
+    if ((!app_ctx) || !(org_img) || (!od_results))
+    {
+        return -1;
+    }
+
+    memset(od_results, 0x00, sizeof(*od_results));
+    memset(&letter_box, 0, sizeof(letterbox_t));
+    memset(inputs, 0, sizeof(inputs));
+    memset(outputs, 0, sizeof(outputs));
+
+    int width   = app_ctx->model_width;
+    int height  = app_ctx->model_height;
+    int channel = app_ctx->model_channel;
+
+    inputs[0].index        = 0;
+    inputs[0].type         = RKNN_TENSOR_UINT8;
+    inputs[0].size         = width * height * channel;
+    inputs[0].fmt          = RKNN_TENSOR_NHWC;
+    inputs[0].pass_through = 0;
+
+    // Pre Process
+    app_ctx->input_image_width = org_img->cols;
+    app_ctx->input_image_height = org_img->rows;
+
+    cv::cvtColor(*org_img, img, cv::COLOR_BGR2RGB);
+    img_width  = img.cols;
+    img_height = img.rows;
+
+    //check sizes
+    if (img_width != width || img_height != height) {
+        cv::resize(img,resized_img,cv::Size(width,height));
+        inputs[0].buf = (void*)resized_img.data;
+    } else {
+        inputs[0].buf = (void*)img.data;
+    }
+
+    ret = rknn_inputs_set(app_ctx->rknn_ctx, app_ctx->io_num.n_input, inputs);
+    if (ret < 0)
+    {
+        printf("rknn_input_set fail! ret=%d\n", ret);
+        return -1;
+    }
+
+    // Run
+    // printf("rknn_run\n");
+    ret = rknn_run(app_ctx->rknn_ctx, nullptr);
+    if (ret < 0)
+    {
+        printf("rknn_run fail! ret=%d\n", ret);
+        return -1;
+    }
+
+    // Get Output
+    memset(outputs, 0, sizeof(outputs));
+    for (uint32_t i = 0; i < app_ctx->io_num.n_output; i++)
+    {
+        outputs[i].index = i;
+        outputs[i].want_float = (!app_ctx->is_quant);
+    }
+    ret = rknn_outputs_get(app_ctx->rknn_ctx, app_ctx->io_num.n_output, outputs, NULL);
+    if (ret < 0)
+    {
+        printf("rknn_outputs_get fail! ret=%d\n", ret);
+        return -1;
+    }
+    // post process
+    float scale_w = (float)width / img_width;
+    float scale_h = (float)height / img_height;
+
+    post_process(app_ctx, outputs, box_conf_threshold, nms_threshold, scale_w, scale_h, od_results);
+
+    // Remeber to release rknn output
+    rknn_outputs_release(app_ctx->rknn_ctx, app_ctx->io_num.n_output, outputs);
+
+    return ret;
+}
